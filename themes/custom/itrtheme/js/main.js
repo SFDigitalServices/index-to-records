16fab1f8b8f53ead9bc0261164f586e54ef919d9
Anthony Kong <anthony.kong@sfgov.org>
2018-02-22 14:27:53 -0800

    toggle menu for schedules

--- themes/custom/itrtheme/js/main.js
+++ themes/custom/itrtheme/js/main.js
@@ -1,1 +1,17 @@
+$ = jQuery;
 console.log('theme js');
+var menu = $('#block-itrtheme-account-menu > .menu');
+var topLevelMenuLinks = $('#block-itrtheme-account-menu > .menu > li > a')
+$(topLevelMenuLinks).click(function(e) {
+  var submenu = $(this).siblings('.menu');
+  if($(submenu).length > 0) {
+    e.preventDefault();
+    console.log('has submenu');
+    $(submenu).toggle();
+    if($(submenu).css('display') == 'none') {
+      $(this).parent('li').removeClass('open');
+    } else {
+      $(this).parent('li').addClass('open');
+    }
+  }
+});
