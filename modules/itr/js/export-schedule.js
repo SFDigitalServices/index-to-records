853c2ff3290531ba07728042c64bcc22579a9c48
Anthony Kong <anthony.kong@sfgov.org>
2018-04-10 08:38:36 -0700

    work on export schedule controller

--- modules/itr/js/export-schedule.js
+++ modules/itr/js/export-schedule.js
@@ -8,4 +8,35 @@ if(drupalSettings.path.currentQuery) {
 } else {
   deptId = drupalSettings.itr.exportSchedule.id;
 }
-console.log('deptId: ' + deptId);
+if(!isNaN(deptId)) {
+  console.log('deptId: ' + deptId);
+  $('#edit-field-department-target-id').val(deptId);
+  var deptInfoUrl = drupalSettings.path.baseUrl + 'dept/info/' + deptId + '?_format=json';
+  // get the department information
+  $.ajax({
+    url: deptInfoUrl,
+    success: function(deptInfo) {
+      if(deptInfo.length > 0) {
+        var dept = deptInfo[0];
+        var deptInfoHtml = '';
+        deptInfoHtml += '<table id="dept-info">';
+        deptInfoHtml += '  <tr>';
+        deptInfoHtml += '   <td><strong>Department Name</strong>:' + $('#edit-field-department-target-id option:selected').text() + '</td>';
+        deptInfoHtml += '   <td><strong>Contact Email</strong>:' + dept.field_department_contact_email[0].value + '</td>';
+        deptInfoHtml += '   <td><strong>Department Website</strong>:' + dept.field_department_website[0].value + '</td>';
+        deptInfoHtml += '  </tr>';
+        deptInfoHtml += '  <tr>';
+        deptInfoHtml += '   <td><strong>Department Contact</strong>:' + dept.field_department_contact_name[0].value + '</td>';
+        deptInfoHtml += '   <td><strong>Contact Phone Number</strong>:' + dept.field_department_contact_phone_n[0].value + '</td>';
+        deptInfoHtml += '   <td></td>';
+        deptInfoHtml += '  </tr>';
+        deptInfoHtml += '</table>';
+
+        $('#views-exposed-form-schedules-page-2').after($('#schedule-export-dept-info'));
+        $('#schedule-export-dept-info').html(deptInfoHtml);
+      }
+    }
+  })
+} else {
+  console.log('do nothing');
+}
--- modules/itr/src/Controller/ExportScheduleController.php
+++ modules/itr/src/Controller/ExportScheduleController.php
@@ -4,10 +4,15 @@
   use Drupal\Core\Controller\ControllerBase;
 
   class ExportScheduleController extends ControllerBase {
-    public function content() {
+    public function content($deptId) {
+      if($deptId == 'all') {
+        $str = 'no dept provided';
+      } else {
+        $str = 'export schedule controller:' . $deptId;
+      }
       return array(
         '#type' => 'markup',
-        '#markup' => $this->t('export schedule controller'),
+        '#markup' => $this->t($str),
       );
     }
   }
