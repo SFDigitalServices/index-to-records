60505a4452964a309d997e13fc1953bfd1e9bb60
Anthony Kong <anthony.kong@sfgov.org>
2018-10-16 10:56:59 -0700

    convert to composer based install

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitDrupal8::getLoader();
+return ComposerAutoloaderInit0e25a6a5e7fe012dc74bbac8d07d5a98::getLoader();
