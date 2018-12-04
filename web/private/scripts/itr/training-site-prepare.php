8a47b9132251b4502972989972e8bcc7152673e7
Anthony Kong <anthony.kong@sfgov.org>
2018-12-03 22:11:57 -0800

    private script

--- web/private/scripts/itr/training-site-prepare.php
+++ web/private/scripts/itr/training-site-prepare.php
@@ -6,3 +6,4 @@ $sitename = \Drupal::config('system.site')->get('name');
 
 echo "This is the site $sitename\n";
 echo "TERMINUS_ENV:".$TERMINUS_ENV."\n";
+echo "PANTHEON_ENV:".$_ENV['PANTHEON_ENVIRONMENT']."\n";
