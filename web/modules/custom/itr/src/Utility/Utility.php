13ceae8a25386f55491b1c814a28d7f75dc17e1d
Anthony Kong <anthony.kong@sfgov.org>
2020-01-15 10:26:40 -0800

    move createCSV to utility

--- web/modules/custom/itr/src/Utility/Utility.php
+++ web/modules/custom/itr/src/Utility/Utility.php
@@ -3,6 +3,7 @@
 
   use Drupal\taxonomy\Entity\Term;
   use Drupal\user\Entity\User;
+  use Drupal\file\Entity\File;
 
   class Utility {
     
@@ -218,6 +219,58 @@
         }
       }
       return null;
+    }
+
+    public static function createCSV(array $data, $deptId = null) {
+      $fileName = isset($deptId) ? str_replace(' ', '-', strtolower(self::getTermNameByTid($deptId))) : 'no-filename';
+  
+      $file = File::create([
+        'filename' => $fileName . '.csv',
+        'uri' => 'public://schedule/export/csv/' . $fileName . '.csv',
+        'status' => 1,
+      ]);
+      $file->save();
+      $dir = dirname($file->getFileUri());
+      if(!file_exists($dir)) {
+        mkdir($dir, 0770, TRUE);
+      }
+      $fp = fopen($file->getFileUri(), 'w');
+  
+      $csvHeaderRow = 'title,link,division,division_contact,on_site,off_site,total,category,retention,remarks'."\n";
+      fwrite($fp, $csvHeaderRow);
+      foreach($data as $item) {
+        $title = $item['field_record_title'][0]['value'];
+        $link = $item['field_link'][0]['value'];
+        $division = count($item['field_division']) > 0 ? self::getTermNameByTid($item['field_division'][0]['target_id']) : '';
+        $division_contact = $item['field_division_contact'][0]['value'];
+        $on_site = $item['field_on_site'][0]['value'];
+        $off_site = $item['field_off_site'][0]['value'];
+        $total = $item['field_total'][0]['value'];
+        $category = count($item['field_category']) > 0 ? self::getTermNameByTid($item['field_category'][0]['target_id']) : '';
+  
+        $retention = '';
+        if(count($item['field_retention']) > 0) {
+          foreach($item['field_retention'] as $retentionItem) {
+            $retentionId = $retentionItem['target_id'];
+            $retention .= self::getRetentionName($retentionId) ? self::getRetentionName($retentionId) . ', ' : '';
+          }
+          $retention = rtrim($retention);
+          $retention = rtrim($retention, ',');
+        }
+  
+        $remarks = $item['field_remarks'][0]['value'];
+        $csvArray = array($title, $link, $division, $division_contact, $on_site, $off_site, $total, $category, $retention, $remarks);
+        fputcsv($fp, $csvArray);
+      }
+      $file->save();
+      fclose($fp);
+      $returnArray = array(
+        array(
+          'filename' => $fileName . '.csv',
+          'url' => $file->url(),
+        )
+      );
+      return $returnArray;
     }
 
   }
